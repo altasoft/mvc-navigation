da96d8b6fc3884cff8a23822af8ccf8eeb71d246
Giorgi Abashidze <giorgi.abashidze@gmail.com>
2015-01-12 18:07:23 +0400

    Update altasoft.navigation.js
    Popup showing up when navigating takes some time

--- altasoft.navigation.js
+++ altasoft.navigation.js
@@ -1,17 +1,25 @@
-﻿// 
+// 
 // Altasoft Navigation Plugin - ajax based navigation for better UX and low traffic load
 // 
 
 (function () {
 
-	var MainContentSelector = '#MainContent';
+    var popupWaitingShowDelay = 500; //ms
+    var popupLongWaitingShowDelay = 1500; //ms
+    var waitingText = "Please wait...";
+    var longWaitingText = "It took longer than expexted";
+
+    var MainContentSelector = '#MainContent';
+
+    var $popup = createWaitingPopup();
+    $(MainContentSelector).after($popup);
 
 	var navigate = function (url, skipAnimation, fadeSelector) {
 
 		$(document).trigger('navigating');
 
 		try {
-			hideContent();
+		    hideContent();		    
 		}
 		catch (err) { }
 
@@ -25,14 +33,15 @@
 	}
 
 	function loadPage(url, skipAnimation, fadeSelector) {
-		if (!url) return;
+		if (!url) return;		
 
 		if (!fadeSelector)
 			fadeSelector = MainContentSelector;
 
-
 		if (!skipAnimation)
-			$(fadeSelector).fadeTo('fast', 0);
+	        $(fadeSelector).fadeTo('fast', 0);
+
+		var ajaxFinished = false;
 
 		$.ajax({
 			url: url,
@@ -40,32 +49,50 @@
 			headers: {
 				"pageonly": true
 			},
-			success: function (data) {
-				successDownloadPage(data)
+			beforeSend: function () {			    
+			    setTimeout(function () {
+			        if (!ajaxFinished) {
+			            showWaitingPopup();
+			        }
+			    }, popupWaitingShowDelay);
+
+			    setTimeout(function () {
+			        if (!ajaxFinished)
+			            setPopupText(longWaitingText);
+			    }, popupLongWaitingShowDelay);
+			    
 			},
+			success: function (data) {
+			    ajaxFinished = true;
+			    $(MainContentSelector).fadeTo('fast', 0, function () {			        
+			        successDownloadPage(data);
+
+			        hideWaitingPopup();
+			    });
+			},            
 			error: function (err) {
+			    ajaxFinished = true;
 				console.log('Fallback, error while loading page', err);
 				window.location.assign(url);
 			}
 		});
 	}
 
-	function successDownloadPage(data) {
-		setTimeout(function () {
-			$(document).scrollTop(0);
-			$(MainContentSelector).empty();
-			$(MainContentSelector).append(data);
-			$(MainContentSelector).fadeTo('fast', 1);
+	function successDownloadPage(data) {	    
+	    setTimeout(function () {	        
+	        $(document).scrollTop(0);	        
+	        $(MainContentSelector).empty();
+	        $(MainContentSelector).append(data);
+	        $(MainContentSelector).fadeTo('fast', 1);
 		}, 200);
 	}
 
-
-
+    
 	onpopstate = function (event) {
 		loadPage(event.state);
 	}
 
-	$(document).on('click', 'a', function () {
+    $(document).on('click', 'a', function () {       
 		var url = $(this).attr('href');
 
 		if (url.indexOf('/') == 0) {
@@ -73,8 +100,54 @@
 
 			return false;
 		}
-	});
+    });
 
+    function createWaitingPopup() {
+        var $popupHtml = $("<div class='requestWaitingMsg' id='popup-modal'></div>");
+
+        $popupHtml.css({
+            position: "fixed",
+            "font-family": "Arial, Helvetica, sans-serif",
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+            background: "rgba(255,255,255,0.8)",
+            "z-index": 10000,
+            opacity: 0,
+            "pointer-events": "none"
+        });
+
+        $popupHtml.append('<div id="msg-content">').find('div#msg-content').
+            css({
+                width: "400px",
+                position: "relative",
+                margin: "10% auto"
+            }).
+            text(waitingText);
+
+        return $popupHtml;
+    };
+
+    function showWaitingPopup() {
+        $popup.css({
+            opacity: 1,
+            display: "block"
+        });
+    };
+
+    function setPopupText(msg) {
+        $popup.children('div').text(msg);        
+    };
+
+    function hideWaitingPopup() {
+        $popup.css({
+            opacity: 0,
+            display: "none"
+        });
+
+        setPopupText(waitingText);        
+    };
 
 	return navigate;
 })();
